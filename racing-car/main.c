584cedc4e98e27976617232cab792ea73d617537
yeeuniii <yeeun@DESKTOP-UKLPSCA.localdomain>
2022-03-25 17:24:56 +0900

    move cars as many times as you enter it

--- racing-car/main.c
+++ racing-car/main.c
@@ -1,39 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 typedef struct
 {
 	int number;
+	int success;
 } Car;
 
-
+void go(int value, Car *car)
+{
+	if (value >= 4)
+	{
+		car -> success ++;
+	}
+}
 
 void main()
 {	
 	int num;
-	int i = 1;
+	int times;
+	int i = 0;
+	int j = 0;
+	int randomvalue;
 
 	printf("How many cars? \n");
 	scanf("%d", &num);
-	
+	printf("How many times are you going to move the cars? \n");
+	scanf("%d", &times);
+
 	Car *cars = malloc(sizeof(Car) * num);
+	char *hyphens = malloc(sizeof(char) * (times + 1));
 	
-	srand(time(NULL));
 	while (i <= num)
 	{	
-		cars[i].number = i;
-		printf("%s%d ", "Car number : ", cars[i].number);
-		
-		if ((rand() % 10 + 1) >= 4)
-		{
-			printf("%c \n", '-');
-		}
-		else
-		{
-			printf("\n");
-		}		
+		cars[i].number = i + 1;
 		i ++;
 	}
 	
+	i = 0;
+	srand(time(NULL));
+	while (i < times)
+	{			
+		printf("%s%d ", "Car number : ", cars[j].number);
+		randomvalue = rand() % 10 + 1;
+		go(randomvalue, &cars[j]);
+		printf("## %d ##", randomvalue);
+		printf("%d \n", cars[j].success);
+		
+		j ++;
+		if (j == num)
+		{
+			printf("\n");
+			j = 0;
+			i ++;
+		}		
+	}
+
 	free(cars);
 }
